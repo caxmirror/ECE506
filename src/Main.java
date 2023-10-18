cfbe712b8f2da100aabe6788d9984c6331e317ba
Liheng <caxmirror@163.com>
2023-10-18 16:03:43 -0500

    test

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: Main
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author Liheng_Zheng
+ * @Create 2023/10/16 18:29
+ * @Version 1.0
+ */
+public class Main {
+}
+//1.packed all the server code I had into jar file
+//2.I have started writting the dockerfile included the only necessary dependency which is openjdk
+//but I guess I need more time to finish that and make sure the dockerfile runs well.
+//JDBC
